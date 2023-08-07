831a10118745719e28b96bc29cb40d162924a2bb
Abrar Ahmed <eersam36@gmail.com>
2023-08-08 02:07:52 +0500

    Initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+
+
+  let typed = new Typed('#element', {
+    strings: ['Front-end Development','Back-end Development','Web design'],
+    typeSpeed: 40,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true
+  });
